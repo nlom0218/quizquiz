4c4aa086a17b8ed14627109f8ed2575c2763b052
nlom0218 <nlom0218@naver.com>
2021-04-19 23:19:56 +0900

    update small scr
    update small scr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router';
 import { connect } from 'react-redux';
 import './CSS/App.css';
@@ -12,7 +12,7 @@ import Quiz from './pages/Quiz';
 import Notice from './pages/Notice';
 
 function App() {
-  return (
+  return (<>
     <div className="App">
       <Switch>
         <Route exact path="/"><Home /></Route>
@@ -23,7 +23,12 @@ function App() {
         <Route path="/quiz"><Quiz /></Route>
       </Switch>
     </div>
-  );
+    <div className="smallScreen">
+      <div className="smallScreen_column">큰 화면으로 키워주세요😅</div>
+      <div className="smallScreen_column">모바일에서는 현재 QUIZ HI이용이 불가능합니다😭</div>
+      <div className="smallScreen_column">앞으로의 업데이트에서 개선하도록 하겠습니다🤓</div>
+    </div>
+  </>);
 }
 
 export default connect()(App);
